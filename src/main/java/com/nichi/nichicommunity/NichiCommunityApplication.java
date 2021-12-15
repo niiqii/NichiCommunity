3ef3188d25afe554e1a00b45f67eea3e5106cc13
Ni7 <syq278872032@gmail.com>
2021-12-15 15:21:14 +0800

    init repo

---
+++ src/main/java/com/nichi/nichicommunity/NichiCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.nichi.nichicommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NichiCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NichiCommunityApplication.class, args);
+    }   
+        
+}
